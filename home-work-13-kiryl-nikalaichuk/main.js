9acdadbfffba443f3e52b6a40f3760f85ce7c7e2
KirylNik <ultramylo@gmail.com>
2018-07-17 15:46:57 +0300

    Added thirteenth tasks.

---
+++ home-work-13-kiryl-nikalaichuk/main.js
@@ -0,0 +1,49 @@
+(function () {
+    const loadDataButtonElement = document.getElementById('loadData'),
+          dataContainerElement = document.getElementById('dataContainer');
+
+    loadDataButtonElement.addEventListener('click', () => {
+        loadData('./users.json', addUsersElementsInContainer, dataContainerElement)
+        const dataBlockElement = createUserDataBlockElement({name: 'Maksim Yakusik', age: 25});
+
+        dataContainerElement.appendChild(dataBlockElement);
+    });
+
+    function loadData(url, callback, ...arg) {
+        const request = new XMLHttpRequest();
+
+        request.open('get', url);
+        request.addEventListener('load', () => {
+            const result = JSON.parse(request.response);
+
+            callback(result, ...arg);
+        });
+
+        request.send();
+    }
+
+    function createUserDataBlockElement(user) {
+        const result = document.createElement('div'),
+              nameElement = document.createElement('span'),
+              ageElement = document.createElement('span');
+
+        nameElement.textContent = user.name;
+        ageElement.textContent = user.age;
+
+        result.appendChild(nameElement);
+        result.appendChild(ageElement);
+
+        return result;
+    }
+
+    function addUsersElementsInContainer (arr, constainer) {
+        const fragment = document.createDocumentFragment();
+
+        arr.forEach(user => {
+            const userElem = createUserDataBlockElement(user);
+            fragment.append(userElem)
+        });
+
+        constainer.append(fragment);
+    }
+})();
